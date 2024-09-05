2ed9705a68c29f34a92e12823e257b1c2061328f
Fabian Klaiber <fklaiber@posteo.net>
2024-09-05 19:23:16 +0200

    Add text to text nodes

--- src/inline.py
+++ src/inline.py
@@ -42,52 +42,72 @@ def extract_markdown_links(text):
 def split_nodes_image(old_nodes):
     new_nodes = []
     for n in old_nodes:
-        images = extract_markdown_images(n.text)
+        if n.text_type != text_type_text:
+            new_nodes.append(n)
+            continue
+
         text = n.text
+        images = extract_markdown_images(text)
+        if len(images) == 0:
+            new_nodes.append(n)
+            continue
+
         for image in images:
             sections = text.split(f"![{image[0]}]({image[1]})", 1)
             if len(sections) != 2:
                 raise ValueError("Invalid markdown, image section not closed")
 
-            nodes = []
-            for i, sec in enumerate(sections):
-                if sec == "":
-                    continue
-                if i % 2 == 0:
-                    node = TextNode(sec, text_type_text)
-                    nodes.append(node)
-                    node = TextNode(image[0], text_type_image, image[1])
-                    nodes.append(node)
-                else:
-                    text = sec
-            new_nodes.extend(nodes)
-    if len(new_nodes) == 0:
-        return old_nodes
+            if sections[0] != "":
+                node = TextNode(sections[0], text_type_text)
+                new_nodes.append(node)
+                node = TextNode(image[0], text_type_image, image[1])
+                new_nodes.append(node)
+
+            text = sections[1]
+
+        if text != "":
+            new_nodes.append(TextNode(text, text_type_text))
+
     return new_nodes
 
 
 def split_nodes_link(old_nodes):
     new_nodes = []
     for n in old_nodes:
-        links = extract_markdown_links(n.text)
+        if n.text_type != text_type_text:
+            new_nodes.append(n)
+            continue
+
         text = n.text
+        links = extract_markdown_links(text)
+        if len(links) == 0:
+            new_nodes.append(n)
+            continue
+
         for link in links:
             sections = text.split(f"[{link[0]}]({link[1]})", 1)
             if len(sections) != 2:
                 raise ValueError("Invalid markdown, link section not closed")
 
-            nodes = []
-            for i, sec in enumerate(sections):
-                if sec == "":
-                    continue
-                if i % 2 == 0:
-                    node = TextNode(sec, text_type_text)
-                    nodes.append(node)
-                    node = TextNode(link[0], text_type_link, link[1])
-                    nodes.append(node)
-                else:
-                    text = sec
-            new_nodes.extend(nodes)
-    if len(new_nodes) == 0:
-        return old_nodes
+            if sections[0] != "":
+                node = TextNode(sections[0], text_type_text)
+                new_nodes.append(node)
+                node = TextNode(link[0], text_type_link, link[1])
+                new_nodes.append(node)
+
+            text = sections[1]
+
+        if text != "":
+            new_nodes.append(TextNode(text, text_type_text))
+
     return new_nodes
+
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
+    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
--- src/test_inline.py
+++ src/test_inline.py
@@ -128,6 +128,26 @@ class TestInlineMarkdown(unittest.TestCase):
         ]
         self.assertListEqual(new_nodes, expected)
 
+    def test_text_to_textnodes(self):
+        nodes = text_to_textnodes(
+            "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        )
+        expected = [
+            TextNode("This is ", text_type_text),
+            TextNode("text", text_type_bold),
+            TextNode(" with an ", text_type_text),
+            TextNode("italic", text_type_italic),
+            TextNode(" word and a ", text_type_text),
+            TextNode("code block", text_type_code),
+            TextNode(" and an ", text_type_text),
+            TextNode(
+                "obi wan image", text_type_image, "https://i.imgur.com/fJRm4Vk.jpeg"
+            ),
+            TextNode(" and a ", text_type_text),
+            TextNode("link", text_type_link, "https://boot.dev"),
+        ]
+        self.assertListEqual(nodes, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
