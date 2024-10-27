083959e8eb70452afe6f605224157ae4f3a928f3
Fabian Klaiber <fklaiber@posteo.net>
2024-10-27 20:49:58 +0100

    Block types

--- src/block.py
+++ src/block.py
@@ -1,3 +1,13 @@
+import re
+
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_olist = "ordered_list"
+block_type_ulist = "unordered_list"
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
@@ -7,3 +17,37 @@ def markdown_to_blocks(markdown):
         block = block.strip()
         filtered_blocks.append(block)
     return filtered_blocks
+
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return block_type_heading
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return block_type_code
+
+    is_quote = True
+    for l in lines:
+        if not l.startswith(">"):
+            is_quote = False
+    if is_quote:
+        return block_type_quote
+
+    is_unordered_list = True
+    for l in lines:
+        if l.startswith("*") or l.startswith("-"):
+            continue
+        else:
+            is_unordered_list = False
+    if is_unordered_list:
+        return block_type_ulist
+
+    is_ordered_list = True
+    for l in lines:
+        if not re.match(r"^\d+\.", l):
+            is_ordered_list = False
+    if is_ordered_list:
+        return block_type_olist
+
+    return block_type_paragraph
--- src/test_block.py
+++ src/test_block.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block import markdown_to_blocks
+from block import *
 
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -21,6 +21,27 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
 * This is another list item""",
         ]
         self.assertListEqual(markdown_to_blocks(block), blocks)
+
+    def test_block_to_block_type(self):
+        self.assertEqual(block_to_block_type("paragraph"), block_type_paragraph)
+        self.assertEqual(block_to_block_type("# header"), block_type_heading)
+        self.assertEqual(block_to_block_type("### header"), block_type_heading)
+        code = """```
+code
+```"""
+        self.assertEqual(block_to_block_type(code), block_type_code)
+
+        quote = """> quote
+> quote"""
+        self.assertEqual(block_to_block_type(quote), block_type_quote)
+
+        unordered_list = """*ulist
+- ulst"""
+        self.assertEqual(block_to_block_type(unordered_list), block_type_ulist)
+
+        ordered_list = """1. olist
+22. olst"""
+        self.assertEqual(block_to_block_type(ordered_list), block_type_olist)
 
 
 if __name__ == "__main__":
